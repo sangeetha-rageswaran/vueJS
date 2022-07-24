ec65ea3eafb5ded0e1a01a67c44a0f61ffbbacec
nagasiva <nagasiva2005@gmail.com>
2022-07-25 00:55:18 +0200

    fixing file path

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventList from '../views/EventList.vue'
+import EventList from '@/views/EventList'
 import EventDetails from '@/views/EventDetails'
-import About from '../views/About.vue'
+import About from '@/views/About'
 
 const routes = [
   {
